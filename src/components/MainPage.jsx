069a606b16f98b9b8fbc69dae342d59bdb9b00ac
Farina Tahsin Chowdhury <farina@Farinas-MacBook-Air.local>
2024-01-02 00:35:17 -0800

    second commit

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Folder from './Folder'
 import './mainpage.css'
 import AddButton from './AddButton'
 
 const MainPage = () => {
-    
-    function handleChange() {
-
+    const [folderArray, setFolderArray] = useState(null)
+    const [numOfFolder, setNumOfFolder] = useState([{}])
+    function handleFolderClick() {
+        // setNumOfFolder(null)
+    }
+    function handleButtonClick() {
+        setNumOfFolder(numOfFolder.push({
+            folder_name: "My Folder",
+            folder_num: numOfFolder.length
+        }))
     }
   return (
     <div className='main-page'>
-        <Folder folderName='My Folder 1' handleChange={handleChange}/>
-        <Folder folderName='My Folder 2'/>
+        {numOfFolder.map((item) => {
+            return (
+                <Folder folderName={`${item.folder_name} ${item.folder_num}`} handleCick={handleFolderClick}/>
+            )
+        })}
+        
+        {/* <Folder folderName='My Folder 2'/>
         <Folder folderName='My Folder 3'/>
-        <Folder folderName='My Folder 4'/>
-        <AddButton />
+        <Folder folderName='My Folder 4'/> */}
+        <AddButton handleClick={handleButtonClick}/>
     </div>
   )
 }
